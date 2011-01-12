6ac256da4d832a862bfba44be7f18432366440fc
Chris Dearman <chris@mips.com>
2011-01-12 15:40:05 -0800

    [DONTMERGE] Disable qemu build for now

--- Android.mk
+++ Android.mk
@@ -2,9 +2,11 @@
 # through the 'm' or 'mm' build commands. if not, we use the
 # standard QEMU Makefile
 #
+ifeq ($(LOCAL_BUILDME),true)
 ifeq ($(DEFAULT_GOAL),droid)
     LOCAL_PATH:= $(call my-dir)
     include $(LOCAL_PATH)/Makefile.android
 else
     include Makefile.qemu
+endif
 endif
