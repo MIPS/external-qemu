3054c87f5e7ce6ee694435f8498793d400eb7ef2
Chris Dearman <chris@mips.com>
2011-12-16 09:00:16 -0800

    [MIPS] Recognise all variants
    For now all variants use the same emulator and kernel
    
    (cherry picked from commit 63e13c345ba082a8c41aebd92cdf8f74d56b854f)
    
    Change-Id: I81f3ce0e2e6ea4270db3e2fe1bab19f5e769d9b3

--- android/avd/util.c
+++ android/avd/util.c
@@ -254,7 +254,7 @@ path_getBuildTargetArch( const char* androidOut )
             result = "arm";
         else if (!strcmp("armeabi-v7a", cpuAbi))
             result = "arm";
-        else if (!strcmp("mips-r2", cpuAbi))
+        else if (!strncmp("mips", cpuAbi, 4))
             result = "mips";
         else
             result = cpuAbi;
